6d0ec130396a916d7b62d3a6185fc13979423ffe
Raman Novakhatski <romabelarus@mail.ru>
2023-05-27 12:17:45 +0300

    create test suite parallel false

--- src/main/java/org/example/pages/RegistrationFormPage.java
+++ src/main/java/org/example/pages/RegistrationFormPage.java
@@ -3,6 +3,8 @@ package org.example.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -11,15 +13,23 @@ public class RegistrationFormPage {
     private WebDriver driver;
     private WebDriverWait wait;
 
-    WebElement firstNameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("firstName")));
+    @FindBy(id = "firstName")
+    private WebElement firstNameField;
+
+    @FindBy(xpath = "//input[@placeholder=\"Last Name\"]")
+    private WebElement lastNameField;
 
     public RegistrationFormPage(WebDriver driver, WebDriverWait wait) {
+        PageFactory.initElements(driver, this);
         this.driver = driver;
         this.wait = wait;
     }
 
-    public void setFirstName(String firstName) {
+    public void enterFirstName(String firstName) {
         firstNameField.sendKeys(firstName);
     }
 
+    public void enterLastName(String lastName) {
+        lastNameField.sendKeys(lastName);
+    }
 }
--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,10 +1,12 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.example.pages.RegistrationFormPage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -15,28 +17,25 @@ import java.time.Duration;
 
 public class FormTest {
 
-    private RegistrationFormPage registrationFormPage;
-
     private WebDriver driver;
     private WebDriverWait wait;
+    private RegistrationFormPage registrationFormPage;
 
     @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        registrationFormPage = new RegistrationFormPage(driver, wait);
         driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         driver.get("https://demoqa.com/automation-practice-form");
+        registrationFormPage = new RegistrationFormPage(driver, wait);
     }
 
-    @Test
+    @Test(priority = 1)
     public void checkRegistrationFormData() {
 
-        registrationFormPage.setFirstName("Roman");
-
-        WebElement lastNameField = driver.findElement(By.xpath("//input[@placeholder=\"Last Name\"]"));
-        lastNameField.sendKeys("Ivanov");
+        registrationFormPage.enterFirstName("Roman");
+        registrationFormPage.enterLastName("Ivanov");
 
         WebElement maleRadioButton = driver.findElement(By.xpath("//label[@for=\"gender-radio-1\"]"));
         maleRadioButton.click();
@@ -44,8 +43,26 @@ public class FormTest {
         WebElement mobileNumberField = driver.findElement(By.xpath("//input[@placeholder='Mobile Number']"));
         mobileNumberField.sendKeys("1111111111");
 
+        WebElement dropDown = driver.findElement(By.id("react-select-3-input"));
+        dropDown.sendKeys("NCR");
+        dropDown.sendKeys(Keys.RETURN);
+
+        WebElement uploadPictureInput = driver.findElement(By.id("uploadPicture"));
+        uploadPictureInput.sendKeys("C:\\Users\\User\\IdeaProjects\\MyFirstTestProject\\src\\main\\resources\\1.png");
+
         WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']")));
         submitButton.click();
+
+        WebElement userDataText = driver.findElement(By.xpath("//tr[1]/td[1]/following-sibling::td"));
+
+        Assert.assertTrue(userDataText.getText().contains("Roman Ivanov"));
+    }
+
+    @Test(priority = 2)
+    public void checkRegistrationFormData2() {
+
+        registrationFormPage.enterFirstName("Roman");
+        registrationFormPage.enterLastName("Ivanov");
 
         WebElement userDataText = driver.findElement(By.xpath("//tr[1]/td[1]/following-sibling::td"));
 
