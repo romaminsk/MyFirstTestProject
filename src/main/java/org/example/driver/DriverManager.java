e9d3e4fb52250e0aaedcb1067e994b029aeae77e
Raman Novakhatski <romabelarus@mail.ru>
2023-05-30 17:15:57 +0300

    add bundle

--- src/main/java/org/example/driver/DriverManager.java
+++ src/main/java/org/example/driver/DriverManager.java
@@ -6,8 +6,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.ResourceBundle;
 
 public class DriverManager {
+
+    static ResourceBundle bundle = ResourceBundle.getBundle("test_framework");
+
     private static WebDriver driver = null;
     public DriverManager() {
     }
@@ -19,7 +23,8 @@ public class DriverManager {
             driver.manage().window().maximize();
             driver.manage().deleteAllCookies();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-            driver.get(PropertiesReader.getProperty("path_to_url"));
+//            driver.get(PropertiesReader.getProperty("path_to_url"));
+            driver.get(bundle.getString("path_to_url"));
         }
         return driver;
     }
