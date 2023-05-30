6a529cd02111253d632e67aa7c9c0e4c4d535ab2
Raman Novakhatski <romabelarus@mail.ru>
2023-05-30 20:41:25 +0300

    add waiters to page

--- src/main/java/org/example/pages/RegistrationFormPage.java
+++ src/main/java/org/example/pages/RegistrationFormPage.java
@@ -31,7 +31,7 @@ public class RegistrationFormPage {
     }
 
     public void enterFirstName(String firstName) {
-//        Waiters.waitForVisibility(firstNameField);
+        Waiters.waitForVisibility(firstNameField);
         firstNameField.sendKeys(firstName);
     }
 
