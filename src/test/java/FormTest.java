4010fd44b1be762ca52678e0e4da3d8074e0bba9
Raman Novakhatski <romabelarus@mail.ru>
2023-05-31 17:22:53 +0300

    add BeforeClass

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,20 +1,25 @@
 import org.example.pages.RegistrationFormPage;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class FormTest extends BaseTest {
+
+    private RegistrationFormPage registrationFormPage;
 
     @DataProvider(name = "data-provider")
     public Object[][] dpMethod() {
         return new Object[][]{{"Roman"}, {"Andrew"}};
     }
 
+    @BeforeClass
+    public void startPage() {
+        registrationFormPage = new RegistrationFormPage(driver);
+    }
+
     @Test(dataProvider = "data-provider")
     public void checkRegistrationFormData(String name) {
-        RegistrationFormPage registrationFormPage;
-        registrationFormPage = new RegistrationFormPage(driver);
-
         registrationFormPage.enterFirstName(name);
         registrationFormPage.enterLastName("Ivanov");
         registrationFormPage.clickMaleRadioButton();
